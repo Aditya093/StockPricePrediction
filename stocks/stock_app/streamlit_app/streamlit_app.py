8720ee0fb4f1342d71aa11bab997d9f098c095bd
Aditya093 <90855562+Aditya093@users.noreply.github.com>
2023-08-21 13:51:55 +0530

    Update streamlit_app.py

--- stocks/stock_app/streamlit_app/streamlit_app.py
+++ stocks/stock_app/streamlit_app/streamlit_app.py
@@ -37,67 +37,67 @@ html="""
            """
 
        
-    st.set_page_config( layout = "wide",
+st.set_page_config( layout = "wide",
     initial_sidebar_state = "collapsed",
 )
-    START=datetime.datetime(2009,1,1)
-    TODAY=datetime.date.today()
-    style.use('dark_background')
-    df=pd.read_csv('C:/Users/Lenovo/Downloads/listing_status.csv')
-    stock=df['symbol']
-    stocks =list(stock)
-    st.title("Stock Prediction")
-    selected_stock=st.selectbox('Select stock for prediction',stocks)
-    n_years=st.slider('Years of Prediction',1,5)
-    period=n_years*365
-    @st.cache_data
-    def load_data(ticker):
-       data=yf.download(ticker,START,TODAY)
-       data.reset_index(inplace=True)
-       return data
+START=datetime.datetime(2009,1,1)
+TODAY=datetime.date.today()
+style.use('dark_background')
+df=pd.read_csv('C:/Users/Lenovo/Downloads/listing_status.csv')
+stock=df['symbol']
+stocks =list(stock)
+st.title("Stock Prediction")
+selected_stock=st.selectbox('Select stock for prediction',stocks)
+n_years=st.slider('Years of Prediction',1,5)
+period=n_years*365
+@st.cache_data
+def load_data(ticker):
+   data=yf.download(ticker,START,TODAY)
+   data.reset_index(inplace=True)
+   return data
 
-    data_load_state=st.text('Loading data...')
-    data=load_data(selected_stock)
-    st.subheader('Raw Data')
-    st.write(data.tail())
+data_load_state=st.text('Loading data...')
+data=load_data(selected_stock)
+st.subheader('Raw Data')
+st.write(data.tail())
 
-    def plot_raw_data():
-       fig=go.Figure()
-       fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'],name='stack_open'))
-       fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'],name='stock_close'))
-       fig.layout.update(title_text='Time Series Data',xaxis_rangeslider_visible=True)
-       fig.update_traces(
-       line=dict(color='#af68de')
+def plot_raw_data():
+   fig=go.Figure()
+   fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'],name='stack_open'))
+   fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'],name='stock_close'))
+   fig.layout.update(title_text='Time Series Data',xaxis_rangeslider_visible=True)
+   fig.update_traces(
+         line=dict(color='#af68de')
         )
-       st.plotly_chart(fig)
+   st.plotly_chart(fig)
 
-    plot_raw_data()    
+plot_raw_data()    
 
-    df_train=data[['Date','Close']]
-    df_train=df_train.rename(columns={'Date':'ds','Close':'y'})
-    model=Prophet()
-    model=model.fit(df_train)
-    future = model.make_future_dataframe(periods=period)  
-    predictions=model.predict(future)
-    st.subheader('Forecast data')
-    st.write(predictions.tail())
-    st.markdown('\n\n')
-    st.write(f'Forecast plot for {n_years} years')
+df_train=data[['Date','Close']]
+df_train=df_train.rename(columns={'Date':'ds','Close':'y'})
+model=Prophet()
+model=model.fit(df_train)
+future = model.make_future_dataframe(periods=period)  
+predictions=model.predict(future)
+st.subheader('Forecast data')
+st.write(predictions.tail())
+st.markdown('\n\n')
+st.write(f'Forecast plot for {n_years} years')
     
-    fig1 =plot_plotly(model,predictions)
-    fig1.layout.update(xaxis_rangeslider_visible=True,
+fig1 =plot_plotly(model,predictions)
+fig1.layout.update(xaxis_rangeslider_visible=True,
                        width=50,height=550)
-    fig1.update_traces(
-       line=dict(color='#af68de')
+fig1.update_traces(
+      line=dict(color='#af68de')
     )
-    st.plotly_chart(fig1,use_container_width=True)
-    st.write("Forecast components")
-    fig2 =plot_components_plotly(model,predictions)
-    fig2.layout.update(xaxis_rangeslider_visible=True,
+st.plotly_chart(fig1,use_container_width=True)
+st.write("Forecast components")
+fig2 =plot_components_plotly(model,predictions)
+fig2.layout.update(xaxis_rangeslider_visible=True,
                        width=1000,height=1100,
                        margin=dict(l=0, r=0, t=20, b=0),)
-    fig2.update_traces(
+fig2.update_traces(
        line=dict(color='#af68de')
     )
-    st.write(fig2)
+st.write(fig2)
     
